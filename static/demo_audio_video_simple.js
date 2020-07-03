5badfa655f21bf63db862f2a0fc5b33eefa4c910
Vasileios Thanasias/LGEEB BS Europe ID Business Department(vasileios.thanasias@lge.com) <vasileios.thanasias@lge.com>
2020-07-03 10:15:22 +0200

    improve intendation

--- static/demo_audio_video_simple.js
+++ static/demo_audio_video_simple.js
@@ -1,17 +1,10 @@
 /*
-
 https://easyrtc.com/docs/easyrtc_gettingStarted.php
 https://easyrtc.com/docs/easyrtc_client_tutorial.php
-
 Note: The EasyApp framework is not designed to work with multiple media streams. It's based on the simplifying assumption that there is only one local media stream. If you want to use multiple multiple media streams, you have to accept the effort of managing their assignment to video tags yourself.
-
 https://demo.easyrtc.com/demos/demo_audio_video_simple.html
-
 https://github.com/youennf/easyrtc/tree/master/server_example
-
 https://github.com/youennf/easyrtc/tree/master/demos
-
-
 */
 var selfEasyrtcid = "";
 var roomParticipants;
@@ -95,24 +88,24 @@ $(document).ready(function () {
                 $("#name-client" + currentConnections).text(callerName);
                 console.log('found empty slot');
             }
- 
+
             if (currentConnections == 1) {
-                    $(".video-container").css({ 'width': '30vw' });
-                    $("#rear-video").css({ left: "10vw" });
-                    $("#far-video1").css({ left: "60vw", visibility: 'visible' });   
+                $(".video-container").css({ 'width': '30vw' });
+                $("#rear-video").css({ left: "10vw" });
+                $("#far-video1").css({ left: "60vw", visibility: 'visible' });
             }
             if (currentConnections == 2) {
-                 $(".video-container").css({ 'width': '27vw' });
-                    $("#rear-video").css({ left: "5vw" });
-                    $("#far-video1").css({ left: "38vw", visibility: 'visible' });
-                     $("#far-video2").css({ left: "70vw", visibility: 'visible' });
+                $(".video-container").css({ 'width': '27vw' });
+                $("#rear-video").css({ left: "5vw" });
+                $("#far-video1").css({ left: "38vw", visibility: 'visible' });
+                $("#far-video2").css({ left: "70vw", visibility: 'visible' });
             }
             if (currentConnections == 3) {
-                    $(".video-container").css({ 'width': '23vw' });
-                    $("#rear-video").css({ left: "1vw" });
-                    $("#far-video1").css({ left: "26vw", visibility: 'visible' });
-                    $("#far-video2").css({ left: "51vw", visibility: 'visible' });
-                    $("#far-video3").css({ left: "76vw", visibility: 'visible' });
+                $(".video-container").css({ 'width': '23vw' });
+                $("#rear-video").css({ left: "1vw" });
+                $("#far-video1").css({ left: "26vw", visibility: 'visible' });
+                $("#far-video2").css({ left: "51vw", visibility: 'visible' });
+                $("#far-video3").css({ left: "76vw", visibility: 'visible' });
             }
             easyrtc.setVideoObjectSrc(video, stream);
         });
@@ -131,62 +124,62 @@ $(document).ready(function () {
             }
             //nobody remained in chat
             if (currentConnections == 1) {
-                    $(".video-container").css({ 'width': '35vw' });
-                    $("#far-video1").css({ visibility: 'hidden' });
-                    $("#far-video2").css({ visibility: 'hidden' });
-                    $("#far-video3").css({ visibility: 'hidden' });
-                    $("#rear-video").css({ left: "10vw" });
+                $(".video-container").css({ 'width': '35vw' });
+                $("#far-video1").css({ visibility: 'hidden' });
+                $("#far-video2").css({ visibility: 'hidden' });
+                $("#far-video3").css({ visibility: 'hidden' });
+                $("#rear-video").css({ left: "10vw" });
             }
             //one person remained in chat
             if (currentConnections == 2) {
-                    $("#rear-video").css({ left: "10vw" });
-                    $(".video-container").css({ 'width': '30vw' });
-                    $("#far-video1").css({ left: "60vw" });
-                    $("#far-video1").css({ visibility: 'visible' });
-                    $("#far-video2").css({ visibility: 'hidden' });
-                    $("#far-video3").css({ visibility: 'hidden' });
-                    if (!isEmptySlot(1)) { console.log('here'); }
-                    else if (!isEmptySlot(2)) {
-                        console.log('here');
-                        document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo2").srcObject
-                        $("#name-client1").text(easyrtc.idToName(callParticipants[2]));
-                        callParticipants[1] = callParticipants[2];
-                        callParticipants[2] = "";
-                        easyrtc.setVideoObjectSrc(document.getElementById("callerVideo2"), "");
-                    }
-                    else if (!isEmptySlot(3)) {
-                        console.log('here');
-                        document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo3").srcObject
-                        $("#name-client1").text(easyrtc.idToName(callParticipants[3]));
-                        callParticipants[1] = callParticipants[3];
-                        callParticipants[3] = "";
-                        easyrtc.setVideoObjectSrc(document.getElementById("callerVideo3"), "");
-                    }
+                $("#rear-video").css({ left: "10vw" });
+                $(".video-container").css({ 'width': '30vw' });
+                $("#far-video1").css({ left: "60vw" });
+                $("#far-video1").css({ visibility: 'visible' });
+                $("#far-video2").css({ visibility: 'hidden' });
+                $("#far-video3").css({ visibility: 'hidden' });
+                if (!isEmptySlot(1)) { console.log('here'); }
+                else if (!isEmptySlot(2)) {
+                    console.log('here');
+                    document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo2").srcObject
+                    $("#name-client1").text(easyrtc.idToName(callParticipants[2]));
+                    callParticipants[1] = callParticipants[2];
+                    callParticipants[2] = "";
+                    easyrtc.setVideoObjectSrc(document.getElementById("callerVideo2"), "");
+                }
+                else if (!isEmptySlot(3)) {
+                    console.log('here');
+                    document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo3").srcObject
+                    $("#name-client1").text(easyrtc.idToName(callParticipants[3]));
+                    callParticipants[1] = callParticipants[3];
+                    callParticipants[3] = "";
+                    easyrtc.setVideoObjectSrc(document.getElementById("callerVideo3"), "");
+                }
             }
             //2 people remained in chat
             if (currentConnections == 3) {
-                    $("#rear-video").css({ left: "5vw" });
-                    //   $("#selfVideo, #callerVideo1, #callerVideo2, #callerVideo3").css({ 'max-width': '27vw', 'max-width': '27vw' });
-                    $(".video-container").css({ 'width': '27vw' });
-                    console.log('2 person remained in chat');
-                    $("#far-video1").css({ left: "38vw" });
-                    $("#far-video2").css({ left: "70vw" });
-                    $("#far-video3").css({ visibility: 'hidden' });
-                    if (isEmptySlot(1)) {
-                        console.log('empty is one ');
-                        document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo3").srcObject
-                        $("#name-client1").text(easyrtc.idToName(callParticipants[3]));
-                        callParticipants[1] = callParticipants[3];
-                    }
-                    else if (isEmptySlot(2)) {
-                        console.log('empty is two ');
-                        document.getElementById("callerVideo2").srcObject = document.getElementById("callerVideo3").srcObject
-                        $("#name-client2").text(easyrtc.idToName(callParticipants[3]));
-                        callParticipants[2] = callParticipants[3];
-                    }
-                    callParticipants[3] = "";
-                    easyrtc.setVideoObjectSrc(document.getElementById("callerVideo3"), "");
-            }     
+                $("#rear-video").css({ left: "5vw" });
+                //   $("#selfVideo, #callerVideo1, #callerVideo2, #callerVideo3").css({ 'max-width': '27vw', 'max-width': '27vw' });
+                $(".video-container").css({ 'width': '27vw' });
+                console.log('2 person remained in chat');
+                $("#far-video1").css({ left: "38vw" });
+                $("#far-video2").css({ left: "70vw" });
+                $("#far-video3").css({ visibility: 'hidden' });
+                if (isEmptySlot(1)) {
+                    console.log('empty is one ');
+                    document.getElementById("callerVideo1").srcObject = document.getElementById("callerVideo3").srcObject
+                    $("#name-client1").text(easyrtc.idToName(callParticipants[3]));
+                    callParticipants[1] = callParticipants[3];
+                }
+                else if (isEmptySlot(2)) {
+                    console.log('empty is two ');
+                    document.getElementById("callerVideo2").srcObject = document.getElementById("callerVideo3").srcObject
+                    $("#name-client2").text(easyrtc.idToName(callParticipants[3]));
+                    callParticipants[2] = callParticipants[3];
+                }
+                callParticipants[3] = "";
+                easyrtc.setVideoObjectSrc(document.getElementById("callerVideo3"), "");
+            }
         });
 
         easyrtc.setRoomOccupantListener(convertListToButtons);
@@ -210,7 +203,7 @@ $(document).ready(function () {
     function loginFailure(errorCode, message) {
         easyrtc.showError(errorCode, message);
     }
-  
+
     // Enabling connect button, on any other client connects to same room. It is called everytime a client is connected to the room, and we need to get it's name, and create a button to make a call 
     function convertListToButtons(roomName, data, isPrimary) {
         if (roomName == null) {
@@ -234,7 +227,6 @@ $(document).ready(function () {
                 i++;
             }
         }
-        setupPeerListener();
     }
 
     // Disconnect the call
